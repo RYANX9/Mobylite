804b97cc5224eba54ede7efda4cadd22e66aba76
Ahmed Messaad <78614946+RYANX9@users.noreply.github.com>
2025-12-20 12:44:08 +0100

    Update route.js

--- app/api/proxy-image/route.js
+++ app/api/proxy-image/route.js
@@ -1,25 +1,51 @@
+// app/api/proxy-image/route.js
+
 import { NextResponse } from 'next/server';
 
+
+
 export async function GET(request) {
+
   const { searchParams } = new URL(request.url);
+
   const imageUrl = searchParams.get('url');
 
+
+
   if (!imageUrl) {
+
     return new NextResponse('Missing URL', { status: 400 });
+
   }
+
+
 
   try {
+
     const response = await fetch(imageUrl);
+
     const blob = await response.blob();
+
     const contentType = response.headers.get('content-type');
 
+
+
     return new NextResponse(blob, {
+
       headers: {
+
         'Content-Type': contentType,
+
         'Cache-Control': 'public, max-age=31536000, immutable',
+
       },
+
     });
+
   } catch (error) {
+
     return new NextResponse('Failed to fetch image', { status: 500 });
+
   }
+
 }
